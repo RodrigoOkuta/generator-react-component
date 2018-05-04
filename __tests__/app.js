1523111ed958f179139e64ceb028e55bb5e5ffe6
Tamara Temple <tamara.temple@reachlocal.com>
2018-05-03 19:49:39 -0500

    Add storybook generator
    - expand prompts

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,13 +4,29 @@ var assert = require("yeoman-assert");
 var helpers = require("yeoman-test");
 
 describe("generator-react-class-component:app", () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({ name: "MyComponent" });
-  });
+  describe("with default responses", () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+        name: "MyComponent"
+      });
+    });
 
-  it("creates files", () => {
-    assert.file(["MyComponent.jsx", "MyComponent.spec.jsx"]);
+    it("creates files", () => {
+      assert.file(["MyComponent.js", "MyComponent.test.js"]);
+    });
+  });
+  describe("with other responses", () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+        name: "MyComponent",
+        addTest: false,
+        addStory: true,
+        storyExt: "storybook"
+      });
+    });
+
+    it("creates files", () => {
+      assert.file(["MyComponent.js", "MyComponent.storybook.js"]);
+    });
   });
 });
